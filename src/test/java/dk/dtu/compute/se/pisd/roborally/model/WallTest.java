a98913047764ce24ba77a2f1a0c0ec3550d30765
Gabriel Christ Haugbøl <gabriel.haugbol@gmail.com>
2021-03-01 13:52:07 +0100

    Flere tests tilføjet

--- src/test/java/dk/dtu/compute/se/pisd/roborally/model/WallTest.java
+++ src/test/java/dk/dtu/compute/se/pisd/roborally/model/WallTest.java
@@ -44,7 +44,28 @@ class WallTest {
         Assertions.assertEquals(Heading.SOUTH, current.getHeading(), "Player 0 should be heading SOUTH!");
     }
 
-    //TODO: change test names
+    @Test
+    void wallOnCurrentSpaceNotBlockingMoveForward(){
+        Board board = gameController.board;
+        Player current = board.getCurrentPlayer();
+        current.getSpace().setWall(Heading.EAST);
+        gameController.moveForward(current);
+
+        Assertions.assertEquals(current, board.getSpace(0, 1).getPlayer(), "Player " + current.getName() + " should beSpace (0,0)!");
+        Assertions.assertEquals(Heading.SOUTH, current.getHeading(), "Player 0 should be heading SOUTH!");
+    }
+
+    @Test
+    void wallOnCurrentSpaceNotBlockingMoveForward2(){
+        Board board = gameController.board;
+        Player current = board.getCurrentPlayer();
+        current.getSpace().setWall(Heading.NORTH);
+        gameController.moveForward(current);
+
+        Assertions.assertEquals(current, board.getSpace(0, 1).getPlayer(), "Player " + current.getName() + " should beSpace (0,0)!");
+        Assertions.assertEquals(Heading.SOUTH, current.getHeading(), "Player 0 should be heading SOUTH!");
+    }
+
     @Test
     void wallOnNextSpaceBlockingMoveForward(){
         Board board = gameController.board;
@@ -53,6 +74,28 @@ class WallTest {
         gameController.moveForward(current);
 
         Assertions.assertEquals(current, board.getSpace(0, 0).getPlayer(), "Player " + current.getName() + " should beSpace (0,0)!");
+        Assertions.assertEquals(Heading.SOUTH, current.getHeading(), "Player 0 should be heading SOUTH!");
+    }
+
+    @Test
+    void wallOnNextSpaceNotBlockingMoveForward(){
+        Board board = gameController.board;
+        Player current = board.getCurrentPlayer();
+        current.board.getNeighbour(current.getSpace(),current.getHeading()).setWall(Heading.WEST);
+        gameController.moveForward(current);
+
+        Assertions.assertEquals(current, board.getSpace(0, 1).getPlayer(), "Player " + current.getName() + " should beSpace (0,1)!");
+        Assertions.assertEquals(Heading.SOUTH, current.getHeading(), "Player 0 should be heading SOUTH!");
+    }
+
+    @Test
+    void wallOnNextSpaceNotBlockingMoveForward2(){
+        Board board = gameController.board;
+        Player current = board.getCurrentPlayer();
+        current.board.getNeighbour(current.getSpace(),current.getHeading()).setWall(Heading.SOUTH);
+        gameController.moveForward(current);
+
+        Assertions.assertEquals(current, board.getSpace(0, 1).getPlayer(), "Player " + current.getName() + " should beSpace (0,1)!");
         Assertions.assertEquals(Heading.SOUTH, current.getHeading(), "Player 0 should be heading SOUTH!");
     }
 }
