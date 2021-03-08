60e37837fd6022c26a64bebb3fea81349ca95c7d
Gabriel Christ Haugbøl <gabriel.haugbol@gmail.com>
2021-03-08 11:47:39 +0100

    kommentar

--- src/main/java/dk/dtu/compute/se/pisd/roborally/view/SpaceView.java
+++ src/main/java/dk/dtu/compute/se/pisd/roborally/view/SpaceView.java
@@ -116,6 +116,7 @@ public class SpaceView extends StackPane implements ViewObserver {
         }
 
         if(space.getConveyor()!=null){
+            //de er ikke helt centered 🤨🤨
             Polygon arrow = new Polygon(0.0, 0.0,
                     15.0, 30.0,
                     30.0, 0.0 );
