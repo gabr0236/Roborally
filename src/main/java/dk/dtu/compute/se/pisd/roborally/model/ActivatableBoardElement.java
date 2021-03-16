f2a0a2034ade9fced0c9a600561f61b15b8bf0d0
Commando Ninja <haugbolg@gmail.com>
2021-03-16 06:27:42 +0100

    commit tomt

--- src/main/java/dk/dtu/compute/se/pisd/roborally/model/ActivatableBoardElement.java
+++
@@ -1,7 +0,0 @@
-package dk.dtu.compute.se.pisd.roborally.model;
-
-import org.jetbrains.annotations.NotNull;
-
-public abstract class ActivatableBoardElement  {
-    public abstract void activateElement();
-}
