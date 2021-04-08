83899a987368aa81f3eaa1521b0538d978d7876a
Gabriel Christ Haugbøl <gabriel.haugbol@gmail.com>
2021-04-08 15:47:41 +0200

    reboot nummerering

--- src/main/java/dk/dtu/compute/se/pisd/roborally/model/Reboot.java
+++ src/main/java/dk/dtu/compute/se/pisd/roborally/model/Reboot.java
@@ -9,8 +9,10 @@ public class Reboot{
 
     public final Heading REBOOT_HEADING;
     private final boolean startField;
+    public final int REBOOT_NUMBER;
 
-    public Reboot(Heading REBOOT_HEADING, boolean startField){
+    public Reboot(Heading REBOOT_HEADING, boolean startField, int number){
+        this.REBOOT_NUMBER=number;
         this.REBOOT_HEADING=REBOOT_HEADING;
         this.startField=startField;
     }
